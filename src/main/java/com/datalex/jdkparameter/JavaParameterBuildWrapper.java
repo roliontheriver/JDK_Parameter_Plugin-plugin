0a53bf3380f55f1c44b12fc4565721de508e368d
Sargemck <baris.batiege@datalex.com>
2013-07-02 10:47:11 +0100

    Testing runtime options

--- src/main/java/com/datalex/jdkparameter/JavaParameterBuildWrapper.java
+++ src/main/java/com/datalex/jdkparameter/JavaParameterBuildWrapper.java
@@ -6,10 +6,13 @@ import hudson.Launcher;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.BuildListener;
+import hudson.model.JDK;
 import hudson.tasks.BuildWrapper;
 import hudson.tasks.BuildWrapperDescriptor;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -20,8 +23,33 @@ import java.io.IOException;
  */
 public class JavaParameterBuildWrapper extends BuildWrapper {
 
+    public List<JDK> getJDKs() {
+        List<JDK> jdks = jenkins.model.Jenkins.getInstance().getJDKs();
+        if(jdks==null)
+            jdks = new ArrayList<JDK>();
+        return jdks;
+    }
+
+    public JDK getJDK(String name) {
+
+
+        if(name==null) {
+            // if only one JDK is configured, "default JDK" should mean that JDK.
+            List<JDK> jdks = getJDKs();
+            if(jdks.size()==1)  return jdks.get(0);
+            return null;
+        }
+        for (JDK j : getJDKs()) {
+            if(j.getName().equals(name))
+                return j;
+        }
+        return null;
+    }
+
+
     @Override
     public Environment setUp(AbstractBuild build, Launcher launcher, BuildListener listener) throws IOException, InterruptedException{
+        String jdkValue = (String)build.getBuildVariables().get("jdk");
 
         EnvVars env = build.getEnvironment(listener);
         env.overrideAll(build.getBuildVariables());
