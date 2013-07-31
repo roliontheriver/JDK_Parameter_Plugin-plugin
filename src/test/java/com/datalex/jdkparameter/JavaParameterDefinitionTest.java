e47f8682b6c47ae05489a7224698c3ce57ca1e63
Sargemck <baris.batiege@datalex.com>
2013-07-31 12:01:17 +0100

    Feature complete

--- src/test/java/com/datalex/jdkparameter/JavaParameterDefinitionTest.java
+++ src/test/java/com/datalex/jdkparameter/JavaParameterDefinitionTest.java
@@ -15,27 +15,27 @@ import java.util.Collection;
  * Time: 2:11 PM
  * To change this template use File | Settings | File Templates.
  */
-public class JavaParameterDefinitionTest {
-
-    @org.junit.Rule
-    public JenkinsRule j = new JenkinsRule();
-
-    @Test
-    public void testGetList() throws IOException {
-
-        JDKInstaller.JDKFamilyList jdks = JDKInstaller.JDKList.all().get(JDKInstaller.JDKList.class).toList();
-        System.out.println(jdks.data.length);
-//        System.out.println("Dummy");
-    }
-
-    @Test
-    public void testSomething() {
-        Collection<String> jdkList  = JavaParameterDefinition.getJDKNames();
-        Assert.assertNotNull(jdkList);
-        Assert.assertTrue(jdkList.isEmpty());
-    }
-
-
+//public class JavaParameterDefinitionTest {
+//
+//    @org.junit.Rule
+//    public JenkinsRule j = new JenkinsRule();
+//
+//    @Test
+//    public void testGetList() throws IOException {
+//
+//        JDKInstaller.JDKFamilyList jdks = JDKInstaller.JDKList.all().get(JDKInstaller.JDKList.class).toList();
+//        System.out.println(jdks.data.length);
+////        System.out.println("Dummy");
+//    }
+//
+//    @Test
+//    public void testSomething() {
+//        Collection<String> jdkList  = JavaParameterDefinition.getJDKNames();
+//        Assert.assertNotNull(jdkList);
+//        Assert.assertTrue(jdkList.isEmpty());
+//    }
+//
+//
 
 
 
