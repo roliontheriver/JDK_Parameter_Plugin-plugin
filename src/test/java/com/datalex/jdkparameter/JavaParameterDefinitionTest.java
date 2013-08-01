50867084f022a4511a451dd25d0c360237f8d9db
Sargemck <baris.batiege@datalex.com>
2013-08-01 11:06:45 +0100

    Feature complete

--- src/test/java/com/datalex/jdkparameter/JavaParameterDefinitionTest.java
+++ src/test/java/com/datalex/jdkparameter/JavaParameterDefinitionTest.java
@@ -15,7 +15,7 @@ import java.util.Collection;
  * Time: 2:11 PM
  * To change this template use File | Settings | File Templates.
  */
-//public class JavaParameterDefinitionTest {
+public class JavaParameterDefinitionTest {
 //
 //    @org.junit.Rule
 //    public JenkinsRule j = new JenkinsRule();
